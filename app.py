e9e69f17721ad4753e590096a6bc1bdbb1b19679
Navneet Mallick <168087871+Navneet-Mallick@users.noreply.github.com>
2026-03-12 19:26:34 +0545

    Enhance UI and recommendation functionality
    Added custom CSS for styling and improved movie recommendation button functionality.

--- app.py
+++ app.py
@@ -2,61 +2,99 @@ import pickle
 import streamlit as st
 import requests
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=7b8b7f8b9e3e9d3b0ab494f8f184edee&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
 
+# ---------- CUSTOM CSS ----------
+st.markdown("""
+<style>
+
+.main-title{
+    text-align:center;
+    font-size:45px;
+    font-weight:bold;
+    color:#ff4b4b;
+}
+
+.dev-name{
+    text-align:center;
+    font-size:18px;
+    color:gray;
+    margin-bottom:30px;
+}
+
+.movie-title{
+    text-align:center;
+    font-size:16px;
+    font-weight:600;
+}
+
+.stButton>button{
+    width:100%;
+    background-color:#ff4b4b;
+    color:white;
+    font-size:18px;
+    border-radius:10px;
+    height:3em;
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+# ---------- HEADER ----------
+st.markdown('<p class="main-title">🎬 Movie Recommender System</p>', unsafe_allow_html=True)
+st.markdown('<p class="dev-name">Developed by <b>Navneet Mallick</b></p>', unsafe_allow_html=True)
+
+
+# ---------- LOAD DATA ----------
+movies = pickle.load(open('models/movie_list.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+
+# ---------- FETCH POSTER ----------
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=7b8b7f8b9e3e9d3b0ab494f8f184edee&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data['poster_path']
+    return "https://image.tmdb.org/t/p/w500/" + poster_path
+
+
+# ---------- RECOMMEND FUNCTION ----------
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    distances = sorted(list(enumerate(similarity[index])), key=lambda x: x[1], reverse=True)
+
     recommended_movie_names = []
     recommended_movie_posters = []
+
     for i in distances[1:6]:
-        # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
-    return recommended_movie_names,recommended_movie_posters
+    return recommended_movie_names, recommended_movie_posters
 
 
-st.header('Content Based Movie Recommender System')
-st.header('Developed By Navneet Mallick')
-movies = pickle.load(open('models/movie_list.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
-
+# ---------- MOVIE SELECT ----------
 movie_list = movies['title'].values
+
 selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
+    "🔎 Search or select a movie",
     movie_list
 )
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+# ---------- BUTTON ----------
+if st.button('Recommend Movies'):
+    
+    with st.spinner("Finding similar movies... 🎥"):
+        names, posters = recommend(selected_movie)
+
+    st.subheader("Recommended Movies")
+
     col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+    cols = [col1, col2, col3, col4, col5]
 
-
-
-
-
-
+    for i in range(5):
+        with cols[i]:
+            st.image(posters[i])
+            st.markdown(f"<p class='movie-title'>{names[i]}</p>", unsafe_allow_html=True)
