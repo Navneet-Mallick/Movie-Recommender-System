fd5d44784918684b21596ccbca00d565866c0e65
Navneet Mallick <168087871+Navneet-Mallick@users.noreply.github.com>
2026-03-12 19:21:31 +0545

    Update similarity model path in app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def recommend(movie):
 
 st.header('Content Based Movie Recommender System')
 movies = pickle.load(open('models/movie_list.pkl','rb'))
-similarity = pickle.load(open('models/similarity.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
